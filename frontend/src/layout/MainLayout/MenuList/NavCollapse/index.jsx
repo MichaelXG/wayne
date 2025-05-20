448723af4fabad8d9f9991a3d486253781dc7ef8
Michael XG <michaelxg@gmail.com>
2025-05-19 22:18:34 -0300

    Ajustes

--- frontend/src/layout/MainLayout/MenuList/NavCollapse/index.jsx
+++ frontend/src/layout/MainLayout/MenuList/NavCollapse/index.jsx
@@ -199,7 +199,8 @@ export default function NavCollapse({ menu, level, parentId }) {
           ...((!drawerOpen || level !== 1) && {
             py: level === 1 ? 0 : 1,
             '&:hover': {
-              bgcolor: 'transparent'
+              bgcolor: theme.palette.grey[300], // cor clara no hover fora do drawer aberto
+              color: theme.palette.text.primary // muda cor do texto no hover
             },
             '&.Mui-selected': {
               bgcolor: 'transparent',
@@ -230,7 +231,8 @@ export default function NavCollapse({ menu, level, parentId }) {
                   alignItems: 'center',
                   justifyContent: 'center',
                   '&:hover': {
-                    bgcolor: theme.palette.grey[300]
+                    bgcolor: theme.palette.grey[300], // cor clara no hover fora do drawer aberto
+                    color: theme.palette.grey[600] // muda cor do texto no hover
                   },
                   ...(isSelected && {
                     bgcolor: theme.palette.grey[300],
